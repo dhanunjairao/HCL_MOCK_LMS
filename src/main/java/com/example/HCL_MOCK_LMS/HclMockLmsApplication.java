e767942eaf7436a38846b4e7bf087703011241c2
dhanunjairao <dhanunjairao_ari@srmap.edu.in>
2026-05-12 11:49:21 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/HCL_MOCK_LMS/HclMockLmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.HCL_MOCK_LMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HclMockLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HclMockLmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HCL_MOCK_LMS/HclMockLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HCL_MOCK_LMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HclMockLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
